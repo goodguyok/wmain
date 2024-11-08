3b8efc11184c9e4ffb6fc1e8988d8bd04f6e3b28
weiyn <2259819653@qq.com>
2024-11-08 16:55:34 +0800

    Fix WSave logic and add inheritance relationship, make it more abstract

--- test_autosave.py
+++ test_autosave.py
@@ -1,38 +1,41 @@
-from wmain.autosave import WAutoSave
+from wmain.autosave import WAutoSave, WSave
     
-class D(WAutoSave):
+class D(WSave):
     def __init__(self):
         self.d = 4
 
-class B(WAutoSave):
+class B(WSave):
 
     def __init__(self):
         self.b = 12
         self.d = D()
         
-class Friend2(WAutoSave):
+class Friend2(WSave):
 
     def __init__(self):
         self.score = 100
 
-class Friend(WAutoSave):
+class Friend(WSave):
 
     def __init__(self):
         self.score = 100
         self.friend2 = Friend2()
 
-class UserIni(WAutoSave):
+class UserIni(WSave):
 
     def __init__(self):
         self.name = "user"
         self.age = 20
         self.gender = "male"
         self.friend = Friend()
+        self.is_good = True
+        self.friends = ["sb", "bb", "cc"]
 
 if __name__ == '__main__':
     user = UserIni()
     user.name = "sb"
-    user.bind_autosave("test_autosave.ini")
+    user.bind_save_file("test_autosave.ini")
     print(user.friend.score)
     user.friend.score = 80
     user.friend.friend2.score = 90
+    user.save()
--- wmain/autosave.py
+++ wmain/autosave.py
@@ -1,75 +1,122 @@
 import json
 import os
-from typing import Iterator
+from typing import Iterator, Dict
 from wmain.base import create_path
 
-__autosave_allow_types__ = (int, float, str)
-__autosave_attr_whitelist__ = [
-    "__autosave_filepath__",
-    "__autosave_callback__",
-    "__dict__",
-]
 
-class WAutoSave:
-
-    def bind_autosave(self, filepath) -> None:
-        self.__autosave_filepath__ = filepath
-        for instance in self.__autosave_iter_instances__():
-            instance.__dict__["__autosave_callback__"] = self.__autosave__
+class WSave:
+    """
+    manual save
+    just allow class attribute type: int, float, str, bool, list, dict
+    >>> class A(WSave):
+    ...     def __init__(self):
+    ...         self.a = 1
+    ...         self.b = [1, 2, 3]
+    ... 
+    ... a = A()
+    ... a.save('test.json')
+    ... a.a = 2
+    ... a.save()
+    ... a.b.append(4)
+    ... a.save()
+    ... a.c = {'a': 1, 'b': 2}
+    ... a.save()
+    
+    """
+    __save_allow_types__ = (int, float, str, bool, list, dict)
+    __save_attr_whitelist__ = [
+        "__save_filepath__",
+        "__save_callback__",
+        "__dict__",
+    ]
+    
+    def bind_save_file(self, filepath: str) -> None:
+        self.__save_filepath__ = filepath
+        for instance in self.__save_iter_instances__():
+            instance.__dict__["__save_callback__"] = self.save
 
         if not os.path.exists(filepath):
-            self.__autosave__()
+            self.save()
         else:
             with open(filepath, "r") as f:
                 attr_dict = dict(json.load(f))
-                self.__autosave_load_attr_dict__(attr_dict)
-
-    def __autosave__(self) -> None:
-        if "__autosave_filepath__" in self.__dict__:
-            create_path(self.__autosave_filepath__)
-            attr_dict = self.__autosave_get_attr_dict__()
-            with open(self.__autosave_filepath__, "w") as f:
+                self.__save_load_attr_dict__(attr_dict)
+                
+    def save(self) -> None:
+        if "__save_filepath__" in self.__dict__:
+            create_path(self.__save_filepath__)
+            attr_dict = self.__save_get_attr_dict__()
+            with open(self.__save_filepath__, "w") as f:
                 json.dump(attr_dict, f)
-        elif "__autosave_callback__" in self.__dict__:
-            self.__autosave_callback__()
+        elif "__save_callback__" in self.__dict__:
+            object.__getattribute__(self, "__save_callback__")()
 
-    def __autosave_iter_instances__(self) -> Iterator["WAutoSave"]:
+    def __save_iter_instances__(self) -> Iterator["WSave"]:
         for v in self.__dict__.values():
-            if isinstance(v, WAutoSave):
+            if isinstance(v, WSave):
                 yield v
-                yield from v.__autosave_iter_instances__()
+                yield from v.__save_iter_instances__()
 
-    def __autosave_get_attr_dict__(self) -> dict:
+    def __save_get_attr_dict__(self) -> dict:
         attr_dict = {}
         for k, v in self.__dict__.items():
-            if k in __autosave_attr_whitelist__:
+            if k in self.__save_attr_whitelist__:
                 continue
-            if isinstance(v, __autosave_allow_types__):
+            if isinstance(v, self.__save_allow_types__):
                 attr_dict[k] = v
-            elif isinstance(v, WAutoSave):
-                attr_dict[k] = v.__autosave_get_attr_dict__()
+            elif isinstance(v, WSave):
+                attr_dict[k] = v.__save_get_attr_dict__()
             else:
                 raise TypeError(
-                    f"Type {type(v)} of Attribute {k} in {type(self)} is not allowed to be autosaved."
+                    f"Type {type(v)} of Attribute {k} in {type(self)} is not allowed to be saved."
                 )
         return attr_dict
 
-    def __autosave_load_attr_dict__(self, attr_dict: dict):
+    def __save_load_attr_dict__(self, attr_dict: dict):
         for k, v in attr_dict.items():
             target_type = type(self.__dict__.get(k))
-            if target_type in __autosave_allow_types__:
+            if target_type in self.__save_allow_types__:
                 self.__dict__[k] = target_type(v)
-            elif issubclass(target_type, WAutoSave):
-                self.__dict__[k].__autosave_load_attr_dict__(v)
+            elif issubclass(target_type, WSave):
+                self.__dict__: Dict[str, WSave]
+                self.__dict__[k].__save_load_attr_dict__(v)
             else:
                 raise TypeError(
-                    f"Type {target_type} of Attribute {k} in {type(self)} is not allowed to be autosaved."
+                    f"Type {target_type} of Attribute {k} in {type(self)} is not allowed to be saved."
                 )
+
+
+class WAutoSave(WSave):
+    """
+    this class will auto save when attribute changed
+    >>> class B(WAutoSave):
+    ...     def __init__(self):
+    ...         self.b = [1, 2, 3]
+    ... 
+    ... class A(WAutoSave):
+    ...     def __init__(self):
+    ...         self.a = B()
+    ... 
+    ... a = A()
+    ... a.a = 2
+    ... a.b.append(4)
+    ... a.c = {'a': 1, 'b': 2}
+
+    Args:
+        WSave (_type_): _description_
+    """
+
+    __save_allow_types__ = (int, float, str, bool)
+    __save_attr_whitelist__ = [
+        "__save_filepath__",
+        "__save_callback__",
+        "__dict__",
+    ]
 
     def __setattr__(self, name, value):
         object.__setattr__(self, name, value)
         if (
             not callable(getattr(self, name, None))
-            and name not in __autosave_attr_whitelist__
+            and name not in self.__save_attr_whitelist__
         ):
-            self.__autosave__()
+            self.save()
